a265c63e6889405628ecf32afb8623bbdb9d3b01
Max R. P. Grossmann <m@max.pm>
2026-03-06 13:42:05 +1100

    Bump version

--- src/appendmuch/__init__.py
+++ src/appendmuch/__init__.py
@@ -24,7 +24,7 @@ __all__ = [
     "within",
 ]
 
-__version_info__ = 0, 0, 1
+__version_info__ = 0, 0, 2
 __version__ = ".".join(map(str, __version_info__))
 __author__ = "Max R. P. Grossmann, Holger Gerhardt"
 __email__ = "appendmuch@grossmann.nexus"
